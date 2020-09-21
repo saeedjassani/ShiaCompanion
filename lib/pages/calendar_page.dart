d44fb5714cc6336ae7fd4f432ccd361a5152744f
Saeed Jassani <saeedjassani@gmail.com>
2020-09-21 17:03:40 -0500

    Improved CalenderPage

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -3,8 +3,8 @@ import 'dart:convert';
 import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
 import 'package:hijri/hijri_calendar.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import '../constants.dart';
 
@@ -20,22 +20,29 @@ class _CalendarPageState extends State<CalendarPage> {
 
   String eventString = "";
 
-  DateTime _currentDate = DateTime.now();
-
-  String month1 = formatDate(DateTime.now(), [M, ", ", yyyy]) +
-      " / " +
-      HijriCalendar.now().toFormat("MMMM, yyyy");
   TextStyle smallText = new TextStyle(fontSize: 11.0);
 
+  CalendarController _calendarController;
+
   _CalendarPageState();
+
+  @override
+  void initState() {
+    super.initState();
+    _updateEventString();
+    _calendarController = CalendarController();
+  }
+
+  @override
+  void dispose() {
+    _calendarController.dispose();
+    super.dispose();
+  }
 
   _updateEventString() async {
     String events = await rootBundle.loadString("assets/events.json");
 
-    List eventsList = json.decode(events);
-    for (var event in eventsList) {
-      eventsMap[event['date']] = event;
-    }
+    eventsMap = json.decode(events);
 
     DateTime now = DateTime.now();
     HijriCalendar _today = HijriCalendar.fromDate(now);
@@ -46,35 +53,81 @@ class _CalendarPageState extends State<CalendarPage> {
 
   @override
   Widget build(BuildContext context) {
-    _updateEventString();
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
           Container(
             margin: EdgeInsets.symmetric(horizontal: 12.0),
-            child: CalendarCarousel(
-              showOnlyCurrentMonthDate: true,
-              todayBorderColor: Colors.blue,
-              todayButtonColor: Colors.transparent,
-              selectedDayBorderColor: Colors.blue[200],
-              selectedDayButtonColor: Colors.blue[200],
-              thisMonthDayBorderColor: Colors.grey,
-              height: screenHeight * 0.58,
-              selectedDateTime: _currentDate,
-              daysHaveCircularBorder: false,
-              headerText: month1,
-              headerTextStyle:
-                  TextStyle(fontSize: screenWidth * 0.0375, color: Colors.blue),
-              onCalendarChanged: (DateTime dateCal) {
-                HijriCalendar newDate =
-                    HijriCalendar.fromDate(dateCal.add(Duration()));
-                setState(() {
-                  month1 = formatDate(dateCal, [M, ", ", yyyy]) +
-                      " / " +
-                      newDate.toFormat("MMMM, yyyy");
-                });
-              },
-              onDayPressed: (DateTime date, List<String> events) {
+            child: TableCalendar(
+              calendarStyle: CalendarStyle(
+                outsideDaysVisible: false,
+                weekendStyle: TextStyle(),
+              ),
+              daysOfWeekStyle: DaysOfWeekStyle(
+                weekendStyle: TextStyle(color: const Color(0xFF616161)),
+              ),
+              headerStyle: HeaderStyle(
+                  titleTextStyle: TextStyle(fontSize: 14),
+                  titleTextBuilder: (date, locale) {
+                    String month1;
+                    HijriCalendar newDate =
+                        HijriCalendar.fromDate(date.add(Duration()));
+                    month1 = formatDate(date, [M, ", ", yyyy]) +
+                        " / " +
+                        newDate.toFormat("MMMM, yyyy");
+                    return month1;
+                  },
+                  formatButtonVisible: false,
+                  centerHeaderTitle: true),
+              builders: CalendarBuilders(
+                dayBuilder: (context, day, events) {
+                  HijriCalendar hDate =
+                      HijriCalendar.fromDate(day.add(Duration()));
+
+                  String tmpDate = getStringFromDate(hDate);
+                  Color dayColor = Colors.transparent;
+
+                  if (eventsMap != null && eventsMap[tmpDate] != null) {
+                    dayColor = eventsMap[tmpDate]['color'] == 0
+                        ? Colors.green[200]
+                        : Colors.red[200];
+                  }
+                  if (_calendarController.isToday(day) &&
+                      dayColor == Colors.transparent)
+                    dayColor = Colors.blue[200];
+                  return Container(
+                      margin: const EdgeInsets.all(2.0),
+                      decoration: BoxDecoration(
+                          color: dayColor,
+                          border: Border.all(
+                              color: _calendarController.isSelected(day)
+                                  ? Colors.blue
+                                  : Colors.grey)),
+                      padding: EdgeInsets.symmetric(horizontal: 4.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                              day.day.toString(),
+                              style: smallText,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: Text(
+                              convertNumberToUrdu(hDate.hDay.toString()),
+                              style: smallText,
+                            ),
+                          ),
+                        ],
+                      ));
+                },
+              ),
+              calendarController: _calendarController,
+              onDaySelected: (date, events) {
                 HijriCalendar newDate = new HijriCalendar.fromDate(
                     date.add(Duration(days: hijriDate)));
 
@@ -83,58 +136,7 @@ class _CalendarPageState extends State<CalendarPage> {
                 if (eventsMap != null && eventsMap[tmpDate] != null) {
                   eventString += "\n\n" + eventsMap[tmpDate]['content'];
                 }
-                setState(() {
-                  _currentDate = date;
-                });
-              },
-              customDayBuilder: (
-                bool isSelectable,
-                int index,
-                bool isSelectedDay,
-                bool isToday,
-                bool isPrevMonthDay,
-                TextStyle textStyle,
-                bool isNextMonthDay,
-                bool isThisMonthDay,
-                DateTime day,
-              ) {
-                HijriCalendar hDate =
-                    HijriCalendar.fromDate(day.add(Duration()));
-
-                String tmpDate = getStringFromDate(hDate);
-
-                Color dayColor = Colors.transparent;
-
-                if (eventsMap != null && eventsMap[tmpDate] != null) {
-                  dayColor = eventsMap[tmpDate]['color'] == 2
-                      ? Colors.green[200]
-                      : Colors.red[200];
-                }
-                // A RenderFlex overflow error occurs when the page is changed (Day views are shrinked, hence causing the error)
-                return Container(
-                  color: dayColor,
-                  padding: EdgeInsets.symmetric(horizontal: 4.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Text(
-                          day.day.toString(),
-                          style: smallText,
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: Text(
-                          convertNumberToUrdu(hDate.hDay.toString()),
-                          style: smallText,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
+                setState(() {});
               },
             ),
           ),
