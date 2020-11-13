f2f2b1c964862e067c9f4e4abf8e42581ec2165d
Saeed Jassani <saeedjassani@gmail.com>
2020-11-13 11:53:29 -0600

    fixing e2e test failure

--- lib/widgets/prayer_times_card.dart
+++ lib/widgets/prayer_times_card.dart
@@ -26,61 +26,63 @@ class PrayerTimesState extends State<PrayerTimesCard> {
             currentLocation.longitude,
             DateTime.now().timeZoneOffset.inMinutes / 60.0)
         : null;
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: ListView.separated(
-        physics: const NeverScrollableScrollPhysics(),
-        separatorBuilder: (BuildContext context, int index) => Divider(
-          height: 2,
-        ),
-        itemCount: _prayerTimes.length,
-        shrinkWrap: true,
-        itemBuilder: (context, position) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                    flex: 1,
-                    child: Text(
-                      "${_prayerNames[position]} :",
-                    )),
-                Expanded(
-                  flex: 1,
-                  child: Text(
-                    "${_prayerTimes[position]}",
-                    textAlign: TextAlign.end,
+    return _prayerTimes != null
+        ? Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ListView.separated(
+              physics: const NeverScrollableScrollPhysics(),
+              separatorBuilder: (BuildContext context, int index) => Divider(
+                height: 2,
+              ),
+              itemCount: _prayerTimes.length,
+              shrinkWrap: true,
+              itemBuilder: (context, position) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                          flex: 1,
+                          child: Text(
+                            "${_prayerNames[position]} :",
+                          )),
+                      Expanded(
+                        flex: 1,
+                        child: Text(
+                          "${_prayerTimes[position]}",
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                      sharedPreferences != null
+                          ? Padding(
+                              padding: const EdgeInsets.only(left: 12.0),
+                              child: InkWell(
+                                  onTap: () {
+                                    inversePref(
+                                        "${_prayerNames[position].toLowerCase()}_notification");
+                                    setUpNotifications();
+                                  },
+                                  child: sharedPreferences.getBool(
+                                              "${_prayerNames[position].toLowerCase()}_notification") ??
+                                          false
+                                      ? Icon(
+                                          Icons.volume_up,
+                                          size: 20,
+                                        )
+                                      : Icon(
+                                          Icons.block,
+                                          size: 20,
+                                        )),
+                            )
+                          : Container(),
+                    ],
                   ),
-                ),
-                sharedPreferences != null
-                    ? Padding(
-                        padding: const EdgeInsets.only(left: 12.0),
-                        child: InkWell(
-                            onTap: () {
-                              inversePref(
-                                  "${_prayerNames[position].toLowerCase()}_notification");
-                              setUpNotifications();
-                            },
-                            child: sharedPreferences.getBool(
-                                        "${_prayerNames[position].toLowerCase()}_notification") ??
-                                    false
-                                ? Icon(
-                                    Icons.volume_up,
-                                    size: 20,
-                                  )
-                                : Icon(
-                                    Icons.block,
-                                    size: 20,
-                                  )),
-                      )
-                    : Container(),
-              ],
+                );
+              },
             ),
-          );
-        },
-      ),
-    );
+          )
+        : Container();
   }
 
   void inversePref(String s) async {
--- test_driver/app.dart
+++ test_driver/app.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:shia_companion/main.dart' as app;
 import 'package:flutter_driver/driver_extension.dart';
 
 main() {
   enableFlutterDriverExtension();
+  WidgetsApp.debugAllowBannerOverride = false;
   app.main();
 }
