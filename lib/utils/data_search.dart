87a90b934a25b814493342b1dec2d3b20e2837b2
Saeed Jassani <saeedjassani@gmail.com>
2021-02-10 09:19:50 -0600

    added search feature for zikr

---
+++ lib/utils/data_search.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:shia_companion/data/uid_title_data.dart';
+import 'package:shia_companion/pages/item_page.dart';
+import 'package:shia_companion/pages/list_items.dart';
+
+class DataSearch extends SearchDelegate<String> {
+  final List<UidTitleData> listWords;
+
+  DataSearch(this.listWords);
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    //Actions for app bar
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = '';
+          })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    //leading icon on the left of the app bar
+    return IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+        ),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // show some result based on the selection
+    final suggestionList = listWords;
+
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        title: Text(listWords[index].title),
+      ),
+      itemCount: suggestionList.length,
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // show when someone searches for something
+
+    final suggestionList = query.isEmpty
+        ? []
+        : listWords
+            .where((p) => p.title.contains(RegExp(query, caseSensitive: false)))
+            .toList();
+
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        onTap: () {
+          if (suggestionList[index].getUId().contains("~")) {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ItemList(
+                        suggestionList[index].getUId().split("~")[1])));
+          } else {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ItemPage(suggestionList[index])));
+          }
+        },
+        title: Text(suggestionList[index].title),
+      ),
+      itemCount: suggestionList.length,
+    );
+  }
+}
