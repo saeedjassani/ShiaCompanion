8c519517c6eed071b9992a1f8de0fc6887cc2443
Saeed Jassani <saeedjassani@gmail.com>
2021-02-20 09:54:25 -0600

    adding tasbeeh counter

--- lib/widgets/tasbeeh_widget.dart
+++ lib/widgets/tasbeeh_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_beep/flutter_beep.dart';
 import 'package:shia_companion/data/uid_title_data.dart';
 import 'package:shia_companion/pages/item_page.dart';
 import 'package:shia_companion/pages/list_items.dart';
@@ -13,28 +14,107 @@ class TasbeehWidget extends StatefulWidget {
 class _TasbeehWidgetState extends State<TasbeehWidget> {
   int counter = 0;
 
+  bool isChecked = true;
+  TextEditingController controller1, controller2, controller3;
+
   @override
   void initState() {
-    counter = sharedPreferences.getInt("key");
+    counter = sharedPreferences.getInt("count") ?? 0;
+    controller1 = TextEditingController(text: "34");
+    controller2 = TextEditingController(text: "67");
+    controller3 = TextEditingController(text: "100");
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
-        child: ExpansionTile(
-          title: Text("Today's Recitations"),
-          children: <Widget>[
-            ListView.separated(
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              separatorBuilder: (BuildContext context, int index) => Divider(),
-              itemCount: 0,
-              itemBuilder: (BuildContext c, int i) => Text("Salam"),
-            )
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tasbeeh Counter"),
+      ),
+      body: InkWell(
+        onTap: () {
+          counter++;
+          if (isChecked &&
+              (counter == int.parse(controller1.text) ||
+                  counter == int.parse(controller2.text) ||
+                  counter == int.parse(controller3.text))) {
+            FlutterBeep.beep();
+          }
+          setState(() {});
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Checkbox(
+                    value: isChecked,
+                    onChanged: (v) {
+                      setState(() {
+                        isChecked = v;
+                      });
+                    },
+                  ),
+                  Text("Enable beep"),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        controller: controller1,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        controller: controller2,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        controller: controller3,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Center(
+                    child: Text("$counter",
+                        style: Theme.of(context).textTheme.headline2)),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          if (counter > 0) counter--;
+                        });
+                      },
+                      child: Text("MINUS ONE")),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          counter = 0;
+                        });
+                      },
+                      child: Text("RESET")),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
