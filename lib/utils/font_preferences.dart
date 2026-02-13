e52ba1605bc33dc9b993b2f27b02f2379090c374
Saeed Jassani <saeedjassani@Saeeds-MacBook-Air.local>
2026-02-13 15:55:50 +0530

    remove unused images, refactor font preferences

--- lib/utils/font_preferences.dart
+++ lib/utils/font_preferences.dart
@@ -2,6 +2,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class FontPreferences {
   static const String _selectedFontKey = 'arabic_font';
+  static const List<String> _validFonts = ['Qalam', 'MeQuran', 'Uthmani'];
+  static const String _defaultFont = 'Qalam';
+
+  static List<String> get validFonts => _validFonts;
+  static String get defaultFont => _defaultFont;
 
   static Future<void> setSelectedFont(String font) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -10,6 +15,17 @@ class FontPreferences {
 
   static Future<String?> getSelectedFont() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getString(_selectedFontKey);
+    String? savedFont = prefs.getString(_selectedFontKey);
+
+    if (savedFont == null) {
+      return null;
+    }
+
+    if (_validFonts.contains(savedFont)) {
+      return savedFont;
+    }
+
+    await prefs.setString(_selectedFontKey, _defaultFont);
+    return _defaultFont;
   }
 }
