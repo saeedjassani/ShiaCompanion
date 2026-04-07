c9525c9074f9ec1eaa1321db1925bcce55176e2d
Saeed Jassani <saeedjassani@gmail.com>
2026-04-07 11:04:14 -0700

    Add web account deletion flow and policy pages

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,24 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:provider/provider.dart';
 import 'package:shia_companion/firebase_options.dart';
+import 'package:shia_companion/pages/delete_account_page.dart';
 import 'package:shia_companion/utils/dark_mode.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:shia_companion/utils/webview_registry.dart' if (dart.library.js_interop) 'package:shia_companion/utils/webview_registry_web.dart';
+import 'package:shia_companion/utils/webview_registry.dart'
+    if (dart.library.js_interop) 'package:shia_companion/utils/webview_registry_web.dart';
 
 import 'constants.dart';
 import 'pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    usePathUrlStrategy();
+  }
 
   final FirebaseApp app = await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -28,6 +35,20 @@ void main() async {
   registerWebViewWebImplementation();
 
   runApp(const MyApp());
+}
+
+enum _AppLaunchDestination {
+  home,
+  deleteAccount,
+}
+
+_AppLaunchDestination _resolveLaunchDestination(Uri uri) {
+  final segments =
+      uri.pathSegments.where((segment) => segment.trim().isNotEmpty).toList();
+  if (segments.length == 1 && segments.first == 'delete-account') {
+    return _AppLaunchDestination.deleteAccount;
+  }
+  return _AppLaunchDestination.home;
 }
 
 class MyApp extends StatelessWidget {
@@ -65,11 +86,14 @@ class MyApp extends StatelessWidget {
           ),
           themeMode:
               darkModeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
-          home: MyHomePage(
-            title: appName,
-            analytics: analytics,
-            observer: observer,
-          ),
+          home: switch (_resolveLaunchDestination(Uri.base)) {
+            _AppLaunchDestination.deleteAccount => const DeleteAccountPage(),
+            _AppLaunchDestination.home => MyHomePage(
+                title: appName,
+                analytics: analytics,
+                observer: observer,
+              ),
+          },
           navigatorObservers: [observer, routeObserver],
         );
       }),
